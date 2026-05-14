4e0417c167affc93f3b65d57ab152ae1fdc5f092
Guiio08 <emguiop@libertadores.edu.co>
2026-05-14 17:55:17 -0500

    fix: add coleccion/:name to server routes to allow dynamic prerendering

--- guiio-frontend/src/app/app.routes.server.ts
+++ guiio-frontend/src/app/app.routes.server.ts
@@ -3,5 +3,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   { path: 'catalogo/:gender', renderMode: RenderMode.Server },
   { path: 'producto/:id', renderMode: RenderMode.Server },
+  { path: 'coleccion/:name', renderMode: RenderMode.Server },
   { path: '**', renderMode: RenderMode.Prerender },
 ];
